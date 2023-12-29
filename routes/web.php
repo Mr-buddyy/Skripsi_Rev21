1fd35b95389c7d9d3df2855775fbad41e32d1ce9
Buddyy <77492530+Mr-buddyy@users.noreply.github.com>
2023-12-29 16:18:59 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
-
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
